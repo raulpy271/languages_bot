89c5f46852ead2748accf82b2b0ac5bfd5af0f05
Raul <raulrufino0@gmail.com>
2021-05-30 10:18:21 -0300

    feat: Add predicate to search in database for languages

--- prolog/search.pl
+++ prolog/search.pl
@@ -1,5 +1,7 @@
 
-:- module(search, [match_language/4]).
+:- module(search, [match_language/4, search_languages/4]).
+
+:- use_module(dataset).
 
 match_language(Language, AreaOfInterest, Paradigm, TypeSystem) :-
     language(_, _, About, _, LanguageTypes, LanguageParadigm, _) = Language,
@@ -20,3 +22,42 @@ match_language(Language, AreaOfInterest, Paradigm, TypeSystem) :-
         )
     ).
   
+search_languages(AreaOfInterest, Paradigm, TypeSystem, LanguagesFound) :-
+    get_languages(Languages),
+    filter_languages(
+        Languages, 
+        AreaOfInterest, 
+        Paradigm, 
+        TypeSystem, 
+        LanguagesFound).
+
+filter_languages([], _, _, _, []).
+filter_languages(
+        [Language | RestLanguages], 
+        AreaOfInterest, 
+        Paradigm, 
+        TypeSystem, 
+        LanguagesFound) :-
+    match_language(Language, AreaOfInterest, Paradigm, TypeSystem),
+    filter_languages(
+        RestLanguages, 
+        AreaOfInterest, 
+        Paradigm, 
+        TypeSystem,
+        RestLanguagesFound),
+    LanguagesFound = [Language | RestLanguagesFound].
+filter_languages(
+        [Language | RestLanguages], 
+        AreaOfInterest, 
+        Paradigm, 
+        TypeSystem, 
+        LanguagesFound) :-
+    not(match_language(Language, AreaOfInterest, Paradigm, TypeSystem)),
+    filter_languages(
+        RestLanguages, 
+        AreaOfInterest, 
+        Paradigm, 
+        TypeSystem,
+        RestLanguagesFound),
+    LanguagesFound = RestLanguagesFound.
+
