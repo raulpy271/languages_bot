55ad232ad6b3a88d4a663c599b479f1d17ea84ed
Raul <raulrufino0@gmail.com>
2021-06-02 08:19:16 -0300

    feat: Add conversation to response for details about a specific language

--- prolog/languages_bot.pl
+++ prolog/languages_bot.pl
@@ -9,7 +9,6 @@
 
 main(_) :-
     text("saudation", Saudation),
-    writeln(""),
     writeln(Saudation),
     conversation.
 
@@ -20,22 +19,36 @@ conversation :-
     writeln(AreaOfInterestText),
     read(AreaOfInterest),
     search_languages(AreaOfInterest, Paradigm, TypeSystem, LanguagesFound),
-    text_languages_found(LanguagesFound, LanguagesFoundText),
-    (LanguagesFoundText \= ""
-        -> writeln(LanguagesFoundText)
-        ; not_found_conversation
+    (LanguagesFound \= []
+        -> select_language_details(LanguagesFound)
+        ; text("not_found", LanguagesNotFoundText),
+          writeln(LanguagesNotFoundText),
+          try_another_language
     ).
 
-not_found_conversation :-
-    text("not_found", LanguagesNotFoundText),
-    writeln(LanguagesNotFoundText),
+select_language_details(LanguagesFound) :-
+    text_languages_found(LanguagesFound, LanguagesFoundText),
+    writeln(LanguagesFoundText),
+    read(LanguageSelectedName),
+    (search_by_name(
+            LanguageSelectedName, 
+            LanguagesFound, 
+            LanguageSelected)
+        -> text_languages_details(LanguageSelected, TextFormated),
+           writeln(TextFormated)
+        ; text("not_found", LanguagesNotFoundText),
+          writeln(LanguagesNotFoundText)
+    ),
+    try_another_language.
+
+try_another_language :-
     text("want_try_another", WantTryAnotherText),
     writeln(WantTryAnotherText),
     read(WantTryAnotherAnswer),
     (is_positive_answer(WantTryAnotherAnswer)
         -> conversation
-        ; text("bye", ByeText),
-          writeln(ByeText)
+        ;  text("bye", ByeText),
+           writeln(ByeText)
     ).
 
 
